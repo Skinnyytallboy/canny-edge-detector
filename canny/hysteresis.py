27184a66d7a9fc4351cc42eaa46f76f37feead52
Skinnyytallboy <m.jalalahmed2004@gmail.com>
2025-10-02 02:52:46 +0500

    feat: Hysteresis thresholding

--- canny/hysteresis.py
+++ canny/hysteresis.py
@@ -2,6 +2,61 @@
 
 
 class Hysteresis:
-    @staticmethod
-    def threshold(image, Th, Tl):
-        pass
+    def __init__(self):
+        self.visited = []
+    
+    def apply(self, magnitude, Th, Tl):
+        height = len(magnitude)
+        width = len(magnitude[0])
+        edges = []
+        for i in range(height):
+            row = []
+            for j in range(width):
+                row.append(0)
+            edges.append(row)
+        
+        self.visited = []
+        for i in range(height):
+            row = []
+            for j in range(width):
+                row.append(False)
+            self.visited.append(row)
+        
+        for i in range(height):
+            magnitude[i][0] = 0
+            magnitude[i][width - 1] = 0
+        for j in range(width):
+            magnitude[0][j] = 0
+            magnitude[height - 1][j] = 0
+        
+        for i in range(1, height - 1):
+            for j in range(1, width - 1):
+                if magnitude[i][j] >= Th and not self.visited[i][j]:
+                    self.follow_edge(magnitude, edges, i, j, Tl)
+        edges_output = []
+        for i in range(height):
+            row = []
+            for j in range(width):
+                row.append(edges[i][j] * 255)
+            edges_output.append(row)
+        
+        return edges_output
+    
+    def follow_edge(self, magnitude, edges, i, j, Tl):
+        height = len(magnitude)
+        width = len(magnitude[0])
+        if i < 0 or i >= height or j < 0 or j >= width:
+            return
+        if self.visited[i][j]:
+            return
+        if magnitude[i][j] < Tl:
+            return
+        edges[i][j] = 1
+        self.visited[i][j] = True
+        neighbors = [
+            (-1, -1), (-1, 0), (-1, 1),
+            (0, -1),           (0, 1),
+            (1, -1),  (1, 0),  (1, 1)
+        ]
+        for di, dj in neighbors:
+            self.follow_edge(magnitude, edges, i + di, j + dj, Tl)
